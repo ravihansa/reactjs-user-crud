d61b1837faace3a3a0f28da1c7ff66fd0f7cbb1d
ravihansa <ravihansad@gmail.com>
2025-02-15 13:53:35 +0530

    Create user logout related changes

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import { AppBar, Link, Toolbar, Typography } from "@mui/material";
 import "../../styles/navbar.css";
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+    const { logout } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
     return (
         <AppBar position="fixed" className="navbar" sx={{ zIndex: 3 }}>
             <Toolbar>
                 <Typography variant="h6" className="navbar-title" sx={{ flexGrow: 1 }}>
                     Dashboard
                 </Typography>
+                <Link underline="none" onClick={handleLogout} className="logout-button">
+                    Logout
+                </Link>
             </Toolbar>
         </AppBar>
     );
