655493de4bdbe47939cc25dbe9174400f118de4e
ravihansa <ravihansad@gmail.com>
2025-02-18 15:40:21 +0530

    Hide layout after the logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import LogInPage from "./pages/login";
 import UsersPage from "./pages/users";
 import RegisterPage from "./pages/register";
 import Layout from "./components/layout/layout";
 import PrivateRoute from "./routes/privateRoute";
-import { AuthProvider } from "./context/authContext";
+import { AuthProvider, AuthContext } from "./context/authContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
@@ -12,19 +12,30 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/login" element={<LogInPage />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/" element={<UsersPage />} />
-              <Route path="/users" element={<RegisterPage />} />
-            </Route>
-            <Route path="*" element={<LogInPage />} />
-          </Routes>
-        </Layout>
+        <AppContent />
       </Router>
     </AuthProvider>
   );
-}
+};
+
+const AppContent = () => {
+  const { token } = useContext(AuthContext);
+  return token ? (
+    <Layout>
+      <Routes>
+        <Route path="/login" element={<LogInPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<UsersPage />} />
+          <Route path="/users" element={<RegisterPage />} />
+        </Route>
+        <Route path="*" element={<UsersPage />} />
+      </Routes>
+    </Layout>
+  ) : (
+    <Routes>
+      <Route path="*" element={<LogInPage />} />
+    </Routes>
+  );
+};
 
 export default App;
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Box } from "@mui/material";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
+import { AuthContext } from "../../context/authContext";
 
 const Layout = ({ children }) => {
+    const { token } = useContext(AuthContext);
+    // If user is not authenticated, hide the layout
+    if (!token) {
+        return <>{children}</>;
+    }
+
     return (
         <Box sx={{ display: "flex" }}>
             <Navbar />
