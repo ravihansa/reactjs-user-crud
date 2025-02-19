795991990155dedce308e2eecab61b1c489257ad
ravihansa <ravihansad@gmail.com>
2025-02-19 15:38:05 +0530

    create axios interceptor to send req header

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,23 +1,39 @@
 import axios from "axios";
 const apiBaseUrl = import.meta.env.VITE_API_URL;
 
+const api = axios.create({
+    baseURL: apiBaseUrl,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
-export const registerUser = async (userData) => {
-    return await axios.post(`${apiBaseUrl}/users`, userData);
-};
-
-export const getAllUsers = async () => {
-    return axios.get(`${apiBaseUrl}/users/list`);
-};
-
-export const updateUser = async (userId, userData) => {
-    return axios.patch(`${apiBaseUrl}/users/${userId}`, userData);
-};
-
-export const deleteUser = async (userId) => {
-    return axios.delete(`${apiBaseUrl}/users/${userId}`);
-};
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export const logInUser = async (userData) => {
     return axios.post(`${apiBaseUrl}/auth/login`, userData);
+};
+
+export const registerUser = async (userData) => {
+    return await api.post(`/users`, userData);
+};
+
+export const getAllUsers = async () => {
+    return api.get(`/users/list`);
+};
+
+export const updateUser = async (userId, userData) => {
+    return api.patch(`/users/${userId}`, userData);
+};
+
+export const deleteUser = async (userId) => {
+    return api.delete(`/users/${userId}`);
 };
